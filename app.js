53776aa8cca2a33e3bca4052f6b8c872822f509e
Kalle Ahlström <hashtagkalski@gmail.com>
2022-06-29 19:01:09 +0300

    fix PORT const

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const apiRouter = require('./api/router')
 const app = express()
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
 const mongo_URL =
   process.env.NODE_ENV === 'test'
     ? process.env.MONGODB_URI_TEST
